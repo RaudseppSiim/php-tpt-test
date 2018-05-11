8ae1b787a43e1b6acb1a0eef12177c58cbf58dcb
Siim Raudsepp <siim.raudsepp@tptlive.ee>
2018-05-11 09:50:51 +0300

    added awesome things yeas

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -4,9 +4,17 @@ use PHPUnit\Framework\TestCase;
 
 final class ExampleTest extends TestCase
 {
+     /**
+     * @group example
+     */
     public function testSomething(): void {
         $this->assertEquals("a", "a");
-    }  
-
+    }
+    /**
+     * @group math
+     */
+    public function testSum(): void {
+        $s = new Math();
+        $this->assertEquals($s->sum(2, 4), 6);
     }
 }
