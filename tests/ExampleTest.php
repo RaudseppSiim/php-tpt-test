ee67ecde0e50806ab243ecd8535527ad03c7fbd4
Siim Raudsepp <siim.raudsepp@tptlive.ee>
2018-05-11 09:32:36 +0300

    Merge

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -4,19 +4,9 @@ use PHPUnit\Framework\TestCase;
 
 final class ExampleTest extends TestCase
 {
-     /**
-     * @group example
-     */
     public function testSomething(): void {
         $this->assertEquals("a", "a");
     }  
-    }
 
-    /**
-     * @group math
-     */
-    public function testSum(): void {
-        $s = new Math();
-        $this->assertEquals($s->sum(2, 4), 6);
     }
 }
